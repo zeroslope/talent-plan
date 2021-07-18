f3a484b207881dcabb86ef9aacafaa52bbfc605d
zeroslope <jsx55242@foxmail.com>
2021-07-18 23:29:22 +0800

    Part 2 & Part 3

--- rakv/src/bin/kvs.rs
+++ rakv/src/bin/kvs.rs
@@ -1,3 +1,40 @@
+use std::process::exit;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    rename_all = "kebab-case", 
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+)]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(about = "Get the string value of a given string key")]
+    Get { key: String },
+    #[structopt(about = "Set the value of a string key to a string")]
+    Set { key: String, value: String },
+    #[structopt(name = "rm", about = "Remove a given key")]
+    Remove { key: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    match opt.cmd {
+        Command::Set { key, value } => unimplemented(),
+        Command::Get { key } => unimplemented(),
+        Command::Remove { key } => unimplemented(),
+    }
+}
+
+fn unimplemented() {
+    eprintln!("unimplemented");
+    exit(1);
 }
