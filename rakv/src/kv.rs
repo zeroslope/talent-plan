e7703808797e16c31f5d3fac877212934566d008
zeroslope <jsx55242@foxmail.com>
2021-07-18 22:50:51 +0800

    Part 1: Make the tests compile

---
+++ rakv/src/kv.rs
@@ -0,0 +1,19 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        panic!();
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!();
+    }
+}
